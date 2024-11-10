0cefa204f738cd251b0ccbbd917afda596ecbb7e
ch3thiya <ac.ravindranathbc@gmail.com>
2024-11-10 16:46:20 +0530

    almost done

--- yahtzee.c
+++ yahtzee.c
@@ -1,6 +1,6 @@
 /* 
 Programmer: Chethiya Ravindranath
-Last Changes Made On: 09/11/2024
+Last Changes Made On: 10/11/2024
 Module: Programming Methodology
 
 Yahtzee Game
@@ -19,11 +19,11 @@ void countScore(int values[], int category, int *onescore);
 void givePoints(int values[], bool *valid, int point_array[3][13], int category, int *onescore);
 int getSum(int values[]);
 void checkValid(int point_array[3][13], bool *valid, bool *two, bool *three, bool *four, bool *smallStraight, bool *largeStraight, bool *yahtzee, int category);
+void updateRepArray(int values[], int rep_array[2][6]);
 
 int main(void)
 {
-    int values[5] = {2,2,3,3,3};
-    int reroll;
+    int values[5] = {};
     int repeats = 0;
     int category = 0;
     char rollagain;
@@ -32,12 +32,13 @@ int main(void)
     int point_array[3][13] = {{1,2,3,4,5,6,7,8,9,10,11,12,13}, {0,0,0,0,0,0,0,0,25,30,40,0,50}, {0,0,0,0,0,0,0,0,0,0,0,0,0}};
     bool two = false, three = false, four = false;
     bool largeStraight = false, smallStraight = false, yahtzee = false;
-    bool valid = false;
+    bool valid = false, correctCategory = true;
 
     int x=0;
-    while (x < 13){    
-        srand(time(0));
+    srand(time(0));
 
+    while (x < 13)
+    {    
         rollDice(values);
 
         while (repeats < 3)
@@ -63,27 +64,39 @@ int main(void)
         printf("Final dice values: ");
         printDice(values);
 
-        printf("Give a category: ");
-        scanf("%d", &category);
 
-        if (category > 0 && category < 7)
-        {
-            countScore(values, category, &onescore);
-        }
-        else
-        {
-            getPatterns(rep_array, &two, &three, &four, &smallStraight, &largeStraight, &yahtzee, category);
-        }
+        do {
+            printf("Give a category: ");
+            scanf("%d", &category);
 
-        if (point_array[2][category-1] == 0)
-        {
-            checkValid(point_array, &valid, &two, &three, &four, &smallStraight, &largeStraight, &yahtzee, category);
-            givePoints(values, &valid, point_array, category, &onescore);
-        }
-        else
-        {
-            printf("Points already given!\n");
-        }
+            if (category < 1 || category > 13)
+            {
+                printf("Invalid category! Please enter a number between 1 and 13.\n");
+                continue;
+            }
+
+            if (category > 0 && category < 7)
+            {
+                countScore(values, category, &onescore);
+            } 
+            else
+            {
+                updateRepArray(values, rep_array);
+                getPatterns(rep_array, &two, &three, &four, &smallStraight, &largeStraight, &yahtzee, category);
+            }
+
+            if (point_array[2][category - 1] != 0)
+            {
+                printf("Points already given! Choose another category.\n");
+            }
+            else
+            {
+                checkValid(point_array, &valid, &two, &three, &four, &smallStraight, &largeStraight, &yahtzee, category);
+                givePoints(values, &valid, point_array, category, &onescore);
+                break; 
+            }
+        } while (1);
+
 
         for (int i = 0; i < 13; i++)
         {
@@ -103,14 +116,7 @@ int main(void)
         }
         printf("\n\n");
 
-        two = false;
-        three = false;
-        four = false;
-        largeStraight = false;
-        smallStraight = false;
-        yahtzee = false;
-        valid = false;
-
+        repeats = 0;
         x++;
     }
 
@@ -144,7 +150,7 @@ void printDice(int values[])
 void rerollDice(int values[])
 {
     int numRerolls;
-    printf("How many dice do you want to reroll (1 to 5)? ");
+    printf("How many dice do you want to reroll (1 to 5)?\n");
     scanf("%d", &numRerolls);
 
     if (numRerolls < 1 || numRerolls > 5) {
@@ -186,56 +192,44 @@ void countScore(int values[], int category, int *onescore)
 
 void getPatterns(int rep_array[2][6], bool *two, bool *three, bool *four, bool *smallStraight, bool *largeStraight, bool *yahtzee, int category)
 {
-    for (int i = 0; i < 6; i++)
-    {
-        if (rep_array[1][i] == 2)
-        {
-            *two = true;
-        }
-        if (rep_array[1][i] == 3)
-        {
-            *three = true;
-        }
-        if (rep_array[1][i] == 4)
-        {
-            *four = true;
-        }
+    *two = *three = *four = *smallStraight = *largeStraight = *yahtzee = false;
+
+    for (int i = 0; i < 6; i++) {
+        if (rep_array[1][i] == 2) *two = true;
+        if (rep_array[1][i] == 3) *three = true;
+        if (rep_array[1][i] == 4) *four = true;
+        if (rep_array[1][i] == 5) *yahtzee = true;
     }
 
-    for (int i = 0; i < 2; i++)
-    {
-        if (rep_array[i] && rep_array[i + 1] && rep_array[i + 2] && rep_array[i + 3])
+    for (int i = 0; i <= 2; i++)
+    { 
+        if (rep_array[1][i] && rep_array[1][i + 1] && rep_array[1][i + 2] && rep_array[1][i + 3])
         {
-            *smallStraight = true;
-            if (rep_array[i + 4])
+            *smallStraight = true; 
+            if (i <= 1 && rep_array[1][i + 4])
             {
                 *largeStraight = true;
             }
         }
     }
 
-    if (rep_array[5] == 5)
-    {
-        *yahtzee = true;
-    }
-
-    if ((category == 7) && three)
+    if ((category == 7) && *three)
     {
         printf("Three of a kind.\n");
     }
-    if ((category == 8) && four)
+    if ((category == 8) && *four)
     {
         printf("Four of a kind.\n");
     }
-    if ((category == 9) && (two && three))
+    if ((category == 9) && (*two && *three))
     {
         printf("Full house.\n");
     }
-    if ((category == 10) && smallStraight)
+    if ((category == 10) && *smallStraight)
     {
         printf("Small Straight.\n");
     }
-    if ((category == 11) && largeStraight)
+    if ((category == 11) && *largeStraight)
     {
         printf("Large Straight.\n");
     }
@@ -243,15 +237,15 @@ void getPatterns(int rep_array[2][6], bool *two, bool *three, bool *four, bool *
     {
         printf("Chance.\n");
     }
-    if ((category == 13) && yahtzee)
+    if ((category == 13) && *yahtzee)
     {
-        printf("YAHTZEE!!");
+        printf("YAHTZEE!!\n");
     }
 }
 
 void givePoints(int values[], bool *valid, int point_array[3][13], int category, int *onescore)
 {
-    if (valid)
+    if (*valid)
     {
         if (category == 9 || category == 10 || category == 11 || category == 13)
         {
@@ -259,10 +253,7 @@ void givePoints(int values[], bool *valid, int point_array[3][13], int category,
         }
         else if (category == 7 || category == 8 || category == 12)
         {
-            if (valid)
-            {
-                point_array[1][category-1] = getSum(values);
-            }
+            point_array[1][category-1] = getSum(values);
             point_array[2][category-1] = 1;
         }
         else
@@ -273,7 +264,7 @@ void givePoints(int values[], bool *valid, int point_array[3][13], int category,
     }
     else
     {
-        printf("The combination is not valid!");
+        printf("The combination is not valid!\n");
         point_array[1][category-1] = 0;
         point_array[2][category-1] = 1;
     }
@@ -292,45 +283,67 @@ int getSum(int values[])
 
 void checkValid(int point_array[3][13], bool *valid, bool *two, bool *three, bool *four, bool *smallStraight, bool *largeStraight, bool *yahtzee, int category)
 {
+    *valid = false;
     {
+        for (int i = 0; i < 7; i++)
+        {
+            if (i == category)
+            {
+                *valid = true;
+            }
+        }
+
         switch (category)
         {
         case 7:
-            if (three);
-                valid = true;
+            if (*three)
+                *valid = true;
             break;
 
         case 8:
-            if (four);
-                valid = true;
+            if (*four)
+                *valid = true;
             break;
 
         case 9:
-            if (two && three);
-                valid = true;
+            if (*two && *three)
+                *valid = true;
             break;
 
         case 10:
-            if (smallStraight);
-                valid = true;
+            if (*smallStraight)
+                *valid = true;
             break;
 
         case 11:
-            if (largeStraight);
-                valid = true;
+            if (*largeStraight)
+                *valid = true;
             break;
 
         case 12:
-            valid = true;
+            *valid = true;
             break;
 
         case 13:
-            if (yahtzee);
-                valid = true;
+            if (*yahtzee)
+                *valid = true;
             break;
     
         default:
             break;
         }
+    }
+}
+
+void updateRepArray(int values[], int rep_array[2][6])
+{
+    for (int i = 0; i < 6; i++)
+    {
+        rep_array[1][i] = 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        rep_array[1][values[i] - 1]++;
     }
 }
